3b5ed303759f94e5665727687f34dadc6ddb91a2
littlepretty <littlepretty@yahoo.cn>
2013-03-12 16:40:18 -0500

    Initial Commit

---
+++ SmartPointers/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SmartPointers
+//
+//  Created by 闫 佳琪 on 3/12/13.
+//  Copyright (c) 2013 littlepretty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
